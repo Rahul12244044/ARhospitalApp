f8f938a1429b06d8e40869d8ea7a38bf07f07540
Rahul12244044 <rahuljajoria2412@gmail.com>
2025-10-27 15:22:30 +0530

    all done

--- adminPanel/src/components/Navbar.jsx
+++ adminPanel/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import {assets} from "../assets/assets.js";
 import {AdminContext} from "../context/adminContext.jsx";
 import {DoctorContext} from "../context/doctorContext.jsx";
 import {useNavigate} from "react-router-dom";
+import { FaHospital } from 'react-icons/fa';
 const Navbar=()=>{
     const {aToken,setAToken}=useContext(AdminContext);
     const {dToken,setdToken}=useContext(DoctorContext);
@@ -17,10 +18,12 @@ const Navbar=()=>{
     return (
         <>
         <div className="flex justify-between items-center px-4 sm:px-10 py-3 border-b border-gray-200  bg-white">
-            <div className="flex items-center gap-2 text-xs">
-                <img className="w-36 sm:w-40 cursor-pointer" src={assets.admin_logo}/>
-                <p className="border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600">{aToken?"Admin":"Doctor"}</p>
-            </div>
+            <div className="flex items-center">
+                  <FaHospital className="text-blue-600 w-10 h-10" /> {/* size 40px */}
+                  <span className="ml-2 font-bold text-2xl text-blue-900">
+                    ARHospital
+                  </span>
+                </div>
             <button onClick={logOut} className="bg-blue-500 text-white text-sm px-10 py-2 rounded-full cursor-pointer">Logout</button>
         </div>
         </>
